0f9d3026399756c3cc289b951ef1e47865a5c156
emiliekyllesbech <emilie.restrup@gmail.com>
2024-10-06 21:37:55 +0200

    har lavet et spørgsmål i quizzen med transport

---
+++ quiz.js
@@ -0,0 +1,16 @@
+//https://medium.com/@codewithashutosh/how-to-make-multiple-choice-quiz-in-html-code-dc5f99c1b0d5 derfra kan man lave en quiz
+
+function checkAnswer(option, isCorrect) {
+    const circles = document.querySelectorAll('.circle');
+    circles.forEach(circle => {
+        circle.style.backgroundColor = '#FFF387'; // reset all circles
+    });
+
+    const circle = option.querySelector('.circle');
+
+    if (isCorrect) {
+        circle.style.backgroundColor = '#6FCF97'; // Grøn for rigtigt svar
+    } else {
+        circle.style.backgroundColor = '#EB5757'; // Rød for forkert svar
+    }
+}
